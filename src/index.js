f1a8ce8308fc99eaaf6bd5cff3d6c2027e685722
Панов Антон <hightemp.unknown@gmail.com>
2023-01-09 07:32:01 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+import * as bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Octokit } from "@octokit/rest";
